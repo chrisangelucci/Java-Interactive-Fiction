f706e13d652deab429ed4d22d153592df994f357
Some Dude Bro <dennis.lego@aceweb.com>
2018-10-18 12:03:35 -0400

    Update Utils.java

--- src/chrisangelucci/interactivefiction/Utils.java
+++ src/chrisangelucci/interactivefiction/Utils.java
@@ -1,7 +1,7 @@
 package chrisangelucci.interactivefiction;
 
 public class Utils {
-
+//we wrote this inc class, it was ok
 	public static String getBetween(String str, char start, char end){
 		return str.substring(str.indexOf(start)+1, str.indexOf(end, str.indexOf(start)+1));
 	}
